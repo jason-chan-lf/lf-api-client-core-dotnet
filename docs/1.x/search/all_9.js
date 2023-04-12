602b22ad4e5e212327e608bfb124330dbcb2004f
jason-chan-lf <jason-chan-lf@users.noreply.github.com>
2023-04-12 17:22:29 +0000

    Update for branch 1.x created by action 4681107987

--- docs/1.x/search/all_9.js
+++ docs/1.x/search/all_9.js
@@ -5,7 +5,7 @@ var searchData=
   ['client_37',['Client',['../namespaceLaserfiche_1_1Api_1_1Client.html',1,'Laserfiche::Api']]],
   ['httphandlers_38',['HttpHandlers',['../namespaceLaserfiche_1_1Api_1_1Client_1_1HttpHandlers.html',1,'Laserfiche::Api::Client']]],
   ['laserfiche_39',['Laserfiche',['../namespaceLaserfiche.html',1,'']]],
-  ['laserfiche_20api_20client_20core_20_2enet_40',['Laserfiche API Client Core .NET',['../index.html',1,'']]],
+  ['laserfiche_20api_20client_20core_20_2enet_20_20_20demo_40',['Laserfiche API Client Core .NET   DEMO',['../index.html',1,'']]],
   ['oauth_41',['OAuth',['../namespaceLaserfiche_1_1Api_1_1Client_1_1OAuth.html',1,'Laserfiche::Api::Client']]],
   ['resources_42',['Resources',['../namespaceLaserfiche_1_1Api_1_1Client_1_1Resources.html',1,'Laserfiche::Api::Client']]],
   ['utils_43',['Utils',['../namespaceLaserfiche_1_1Api_1_1Client_1_1Utils.html',1,'Laserfiche::Api::Client']]]
